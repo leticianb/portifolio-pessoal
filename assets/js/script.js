59181609de3ffc7c660a9a8fe5537e6e1b573182
leticianb <leticianakahara10@gmail.com>
2024-07-19 00:02:12 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,7 @@ const rootHtml = document.documentElement;
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    if (currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
-    else rootHtml.setAttribute("data-theme", "dark");
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
     toggleTheme.classList.toggle("bi-brightness-high");
     toggleTheme.classList.toggle("bi-moon-stars");
